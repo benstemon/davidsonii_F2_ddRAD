05a1327fda76ac220b6834d4868055bbc54bd636
benstemon <98973194+benstemon@users.noreply.github.com>
2022-02-05 15:52:18 -0500

    Create run_fastp.sh

---
+++ scripts/preprocessing/run_fastp.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#SBATCH -N 1
+#SBATCH -n 16
+#SBATCH -p wessinger-48core
+#SBATCH --job-name=testrun_fastp
+
+cd $SLURM_SUBMIT_DIR
+
+
+#path to fastp
+fastpdir="/work/bs66/software"
+
+#path to demultiplexed files
+demuxdir="/work/bs66/davidsonii_mapping/demuxed"
+
+
+#fastp for loop
+for r1in in $demuxdir/*.F.fq.gz; 
+do
+    r2in="${r1in/F.fq.gz/R.fq.gz}"
+    r1out="${r1in##*/}"
+    r2out="${r1out/F.fq.gz/R.fq.gz}"
+    $fastpdir/./fastp -i "$r1in" -I "$r2in" -o "${r1out/F.fq/F.postfastp.fq}" -O "${r2out/R.fq/R.postfastp.fq}" -y --length_required 30 -c -w 16 -D --cut_front -W 22 --cut_front_mean_quality 15 -h "${r1out/F.fq.gz/html}" -j "${r1out/F.fq.gz/json}"
+done
+
