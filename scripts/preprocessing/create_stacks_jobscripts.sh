30824c8b31301e5ecee345c67e9d2e72646a1714
Ben Stone <98973194+benstemon@users.noreply.github.com>
2022-02-14 16:13:36 -0500

    Update create_stacks_jobscripts.sh

--- scripts/preprocessing/create_stacks_jobscripts.sh
+++ scripts/preprocessing/create_stacks_jobscripts.sh
@@ -5,7 +5,7 @@ jobscriptpath="/work/bs66/davidsonii_mapping/preprocessing_v3/stacks_jobscripts"
 for r1in in $infilepath/*R1.fq.gz;
 do
     r1name="${r1in##*/}"
-    cat base_script.sh > $jobscriptpath/$r1name
+    cat base_script.txt > $jobscriptpath/$r1name
     r2in="${r1in/R1/R2}"
     echo "process_radtags --paired -1 $r1in -2 $r2in -i gzfastq -o $outfilepath -c -q -w 0.15 -s 20 --len_limit 30 --disable_rad_check" >> $jobscriptpath/$r1name
     mv $jobscriptpath/$r1name $jobscriptpath/stacks_"${r1name/_R1.fq.gz/.sh}"
