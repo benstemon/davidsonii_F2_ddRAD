83be71af2c4f5fbc723ca2e9997595be8776db79
Ben Stone <98973194+benstemon@users.noreply.github.com>
2022-02-17 14:54:10 -0500

    Add files via upload

---
+++ scripts/vcf_filtering/4.filterby_gq_dp.sh
@@ -0,0 +1,10 @@
+#these calculations are very quick and do not require jobs to be sumbitted
+#can be performed easily on interactive node
+
+module load vcftools/0.1.17
+
+vcffile='/work/bs66/davidsonii_mapping/vcf_filtering/bestsnps_cohort_addheader.vcf'
+outfile='/work/bs66/davidsonii_mapping/vcf_filtering'
+
+#filter low GQ and DP genotypes 
+vcftools --vcf $vcffile --minGQ 20 --minDP 2 --recode --recode-INFO-all --out $outfile/finalized_snps
